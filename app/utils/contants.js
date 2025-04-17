1e5c976cdcaaec2739d5dc22ba2dc3c87d316493
aman <aman091299@gmail.com>
2025-04-17 19:26:26 +0530

    adding s on http

--- app/utils/contants.js
+++ app/utils/contants.js
@@ -5,7 +5,7 @@ export const YOUTUBE_VIDEOS_API=
 "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyChtM2_7eLXjggTGBy6RHIKtm7wY-aD1-4"
 
 
-export const YOUTUBE_SUGGESTION_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const YOUTUBE_SUGGESTION_API="https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
 
 export const randomName=(length)=> {
