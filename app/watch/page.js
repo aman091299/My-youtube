92f4c7fba4f91bbf1df663f9597e1ce870a99705
aman <aman091299@gmail.com>
2025-04-17 19:10:32 +0530

    adding supense

--- app/watch/page.js
+++ app/watch/page.js
@@ -1,29 +1,12 @@
-'use client'
+import React, { Suspense } from "react";
+import WatchClient from "../components/WatchClient";
 
-import { useSearchParams } from "next/navigation"
-import LiveChat from "../components/LiveChat";
-const page = () => {
-    const searchParams=useSearchParams();
-    const videoId=searchParams.get('v');
+const Page = () => {
+  return (
+    <Suspense fallback={<div>Loading video...</div>}>
+      <WatchClient />
+    </Suspense>
+  );
+};
 
-  return videoId?(
-    <div className="flex w-full gap-4">
-  <iframe
-          width="800"
-          height="500"
-          className="w-12/10"
-          src={"https://www.youtube.com/embed/" + videoId}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-        <LiveChat/>
-    </div>
-  ):(
-    <div className="absolute font-bold text-6xl flex justify-center items-center w-full h-full">
-        VIDEO IS NOT AVAILABLE
-    </div>
-  )
-}
-
-export default page
+export default Page;
